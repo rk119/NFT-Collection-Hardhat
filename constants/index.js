4bf97b042479d0e419825bbcc2305841e7363c02
rk119 <rk119@hw.ac.uk>
2022-06-12 19:44:46 +0400

    Updated

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x2faB37595761a8a1EF7Eb4174b99D46fB28f3117";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-frontend-ecru.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
